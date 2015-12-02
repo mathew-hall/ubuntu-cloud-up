cfd54cac6c2295bd929bf29dc99f1f64b6c4b62a
Mathew Hall <mathew.james.hall@gmail.com>
2015-12-02 15:28:03 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# Ubuntu Cloud KVM
+
+This script automatically creates and launches an Ubuntu cloud instance locally within KVM. It uses a single Ubuntu Cloud image and maps layers writes to separate disks. Multiple VMs share the same cloud image and write to their own delta disks. The script also sets up a static IP assignment and creates entries in `/etc/hosts` for each guest.
+
+Once created, machines can be managed through `virsh` or `virt-manager` (or any `libvirt`-aware tool).
+# Usage
+
+Run using `./make_image.sh machine_name`. The script uses some **insecure** defaults that can be changed by modifying the script or exporting them before running it.
+
+## Defaults
+
+The script will setup AMD64 Ubuntu 14.04 images by default.
+
+The created machines will be given 4 virtual CPUs and 1GB of RAM, with a maximum of 16GB.
+
+The machine will be given an IP in the /24 range for the host IP of the `default` KVM QEMU network, starting from 2. The script looks at existing static mappings to choose the first available IP. For example, if the QEMU virtual network has the IP address 192.168.122.1, the first guest will be assigned 192.168.122.2.
+
+The username defaults to `ubuntu` and the password is set to the machine name by default.
+
+## Customisation
+
+The script uses a NoCloud ISO image to set configuration parameters. You can change these to have the VMs set themselves up differently. The supplied config leaves password authentication open for SSH and doesn't enforce password changes.
+
+## Insecure by Default
+
+The defaults shipping with the script make the machines very insecure. The default `ubuntu` username and the likelihood that machine names (and therefore passwords) are short/dictionary words mean these VMs need hardening before they are exposed to the network. The machines won't be reachable externally after being set up (they're on a private network with NAT), but change passwords and disable password authentication before exposing their SSH daemons.
+
+## Issues 
+
+###Cleaning up
+
+The script doesn't clean up after itself. Machines (and their images) can be safely deleted but the DHCP reservations and hosts will persist. To clean these up, use `virsh net-edit default` to remove the `<host>` entries for removed machines, and remove the matching line from `/etc/hosts`.
+
+###Reusing machine names
+
+The script will break if machines with the same name are created. The script will refuse to create the machine if the disk image is found, but if you delete a machine and subsequently try to create another machine with the same name without deleting the host entries in the network configuration, the script will fail. To fix this, make sure the host entries are deleted (see previous point).
+
+# Example
+
+		 mat@kvm-server:~$ ./make_image.sh test6
+		 Machine Details
+		 Name: test6
+		 UUID: dd9b630c-dea2-4411-be71-a295ce859283
+		 MAC Address: 52:54:00:7e:2c:a7
+		 IP Address: 192.168.122.11
+		 Hostname: virtual-test6 (set in /etc/hosts)
+		 Username: ubuntu
+		 Password: test6
+
+The machine can then be accessed via `ssh ubuntu@virtual-test6` using the password `test6`.
---
+++ make_image.sh
@@ -0,0 +1,128 @@
+#!/usr/bin/env bash
+
+set -e
+#set -x 
+
+VERSION=14.04
+ARCH=amd64
+BASE_IMG=ubuntu-$VERSION-server-cloudimg-$ARCH-disk1.img
+BASE_DIR=${BASE_DIR-/var/lib/libvirt/images}
+
+DISKSIZE=${DISKSIZE-200G}
+
+UUID=$(uuidgen)
+NAME=$1
+MAC=52:54:00$(dd if=/dev/urandom bs=1 count=3 2>/dev/null | xxd -p | sed 's/\(..\)/:\1/g')
+INIT=init-$NAME
+PASSWORD=${PASSWORD-$NAME}
+
+function say {
+	if [ -n "$DEBUG" ]; then
+		echo "[+]" $@
+	fi
+}
+
+if [ -z "$1" ]; then
+	echo "Usage: $0 disk name"
+fi
+
+if [ -f "${BASE_DIR}/$1.img" ]; then
+	echo "${BASE_DIR}/$1.img already exists"
+	exit -1
+fi
+
+# Fetch the base image if it doesn't already exist
+if [ ! -d ${BASE_DIR}/base ]; then
+	echo "[+] Creating ${BASE_DIR}/base"
+	sudo mkdir -p ${BASE_DIR}/base
+fi
+if [ ! -f ${BASE_DIR}/base/${BASE_IMG} ]; then
+	echo "[+] Fetching Ubuntu ${VERSION} ${ARCH} cloud image"
+	URL=http://cloud-images.ubuntu.com/server/releases/14.04/release/${BASE_IMG}
+	DIR=$(mktemp -d image.XXXX)
+	wget ${URL} -O ${DIR}/${BASE_IMG}
+	qemu-img resize ${DIR}/${BASE_IMG} +${DISKSIZE} > /dev/null
+	sudo mv ${DIR}/${BASE_IMG} ${BASE_DIR}/base/${BASE_IMG}
+	rm -r "${DIR}/${BASE_IMG}"
+	echo "[+] Image stored at ${BASE_DIR}/base/${BASE_IMG}"
+	
+fi
+
+
+if [ ! -d ${BASE_DIR}/init ]; then
+	sudo mkdir -p ${BASE_DIR}/init
+fi
+
+if ! virsh net-info default >/dev/null 2>&1; then 
+	echo "[!] Virtual network default not found"
+	echo "    Create a virtual network (using virt-manager) with the following settings:"
+	echo "    Name: default"
+	echo "    Network: (any subnet)"
+	echo "    Enable DHCP"
+	echo "    Start/End should be (subnet).2 and (subnet).254"
+	echo "    Forward to any physical device with NAT"
+	exit -1
+fi
+
+IP_ADDR=$(ifconfig $(virsh net-info default | grep Bridge | awk '{print $2}') | grep "inet addr" | head -n 1 | cut -d : -f 2 | awk '{print $1}')
+IF_SUBNET=${IP_ADDR%.*}
+SUBNET=${SUBNET-$IF_SUBNET}
+
+say "Using net prefix $SUBNET"
+
+N=$(virsh net-dumpxml default | grep host | cut -d = -f 4 | tr -d \' | cut -d . -f 4 | tr -d /\> | awk '$0>x{x=$0};END{print x}')
+if [ ${N:-0} -eq 0 ]; then
+	N=1
+fi
+N=$(( N + 1 ))
+IP=${SUBNET}.$N
+
+DIR=$(mktemp -d init.XXXX)
+
+say "Generating NoCloud config ISO"
+sed -e s/\\\$NAME\\\$/$NAME/g meta-data.template > ${DIR}/meta-data
+sed -e s/\\\$NAME\\\$/$NAME/g -e s/\\\$PASSWORD\\\$/$PASSWORD/g user-data.template > ${DIR}/user-data
+
+genisoimage -output ${DIR}/${INIT}.iso -volid cidata -joliet -rock ${DIR}/user-data ${DIR}/meta-data > /dev/null 2>&1
+
+sudo mv ${DIR}/${INIT}.iso ${BASE_DIR}/init/$INIT.iso
+
+say "Creating root disk image"
+
+qemu-img create -f qcow2 -b ${BASE_DIR}/base/${BASE_IMG}-base ${DIR}/${NAME}.img >/dev/null 2>&1
+
+sudo mv ${DIR}/${NAME}.img ${BASE_DIR}/${NAME}.img
+
+say "Creating machine descriptor"
+
+sed -e s/\\\$NAME\\\$/$NAME/g \
+-e s/\\\$UUID\\\$/$UUID/g \
+-e s/\\\$MAC\\\$/$MAC/g \
+-e s/\\\$INIT\\\$/$INIT/g \
+template.xml > "${DIR}/${NAME}.xml"
+
+say "Adding static IP assignment"
+
+virsh net-update --network default \
+add-last ip-dhcp-host \
+--xml "<host mac='$MAC' name='$NAME' ip='$IP' />" --live --config  >/dev/null
+
+say "Creating and booting VM"
+
+virsh define ${DIR}/$NAME.xml  >/dev/null
+virsh start $NAME >/dev/null
+
+rm -r "${DIR}"
+
+say "Creating static IP mapping in /etc/hosts"
+
+echo "$IP	virtual-$NAME" | sudo tee -a  /etc/hosts > /dev/null
+
+echo "Machine Details"
+echo "Name: $NAME"
+echo "UUID: $UUID"
+echo "MAC Address: $MAC"
+echo "IP Address: $IP"
+echo "Hostname: virtual-$NAME (set in /etc/hosts)"
+echo "Username: ubuntu"
+echo "Password: $PASSWORD"
